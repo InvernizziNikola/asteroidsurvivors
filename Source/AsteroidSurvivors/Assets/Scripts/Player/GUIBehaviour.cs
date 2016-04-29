b78952400d4580969e37205e8b941bcb576a8d1e
Tanaxis <nikola_invernizzi@hotmail.com>
2016-04-29 17:43:06 +0200

    Updated UI Logic

--- Source/AsteroidSurvivors/Assets/Scripts/Player/GUIBehaviour.cs
+++ Source/AsteroidSurvivors/Assets/Scripts/Player/GUIBehaviour.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUIBehaviour : MonoBehaviour {
 
-
-
+    public GameObject MenuBuildings;
+    public GameObject BuildButton;
 
 	// Use this for initialization
 	void Start () {
@@ -15,4 +16,15 @@ public class GUIBehaviour : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void CloseMenuBuildings()
+    {
+        BuildButton.SetActive(true);
+        MenuBuildings.SetActive(false);
+    }
+    public void OpenMenuBuildings()
+    {
+        MenuBuildings.SetActive(true);
+        BuildButton.SetActive(false);
+    }
 }
